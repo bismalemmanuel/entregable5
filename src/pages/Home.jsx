b4f933fbe6aafc939cce0a96d64eea2072f514b0
Emmanuel D <informaticasaba@gmail.com>
2023-01-03 20:56:11 -0400

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { setTrainerGlobal } from '../store/slices/trainer.slice'
+
+const Home = () => {
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const handleSumit = e =>{
+        e.preventDefault()
+        dispatch(setTrainerGlobal(e.target.name.value.trim()))
+        e.target.name.value = ''
+        navigate('/pokedex')
+    }
+
+  return (
+    <div>
+        <img src="/Home/pokedex.png" alt="" />
+        <h1>Hi Trainer!</h1>
+        <p>Give me your name to start</p>
+        <form onSubmit={handleSumit}>
+            <input id='name' type="text" />
+            <button>Start</button>
+        </form>
+    </div>
+  )
+}
+
+export default Home

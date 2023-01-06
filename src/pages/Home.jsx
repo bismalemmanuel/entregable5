f0bd8e58d288fc93c4eb51b11b3bb0ebf465f92b
Emmanuel D <informaticasaba@gmail.com>
2023-01-06 11:59:33 -0400

    maquetacion de pagina principal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,29 +2,41 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { setTrainerGlobal } from '../store/slices/trainer.slice'
+import './styles/home.css'
 
 const Home = () => {
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-    const handleSumit = e =>{
-        e.preventDefault()
-        dispatch(setTrainerGlobal(e.target.name.value.trim()))
-        e.target.name.value = ''
-        navigate('/pokedex')
-    }
+  const handleSumit = e => {
+    e.preventDefault()
+    dispatch(setTrainerGlobal(e.target.name.value.trim()))
+    e.target.name.value = ''
+    navigate('/pokedex')
+  }
 
   return (
-    <div>
-        <img src="/Home/pokedex.png" alt="" />
-        <h1>Hi Trainer!</h1>
-        <p>Give me your name to start</p>
-        <form onSubmit={handleSumit}>
-            <input id='name' type="text" />
-            <button>Start</button>
-        </form>
-    </div>
+    <div className='article__home'>
+      <img src="/Home/pokedex.png" alt="" />
+           
+      <h1>Hi Trainer!</h1>
+      <p>Give me your name to start</p>
+      <form onSubmit={handleSumit} className='form__home' >
+        <input id='name' type="text" placeholder='tu nombre...' />
+        <button>Start</button>
+      </form>
+      
+      <footer className='footer Principal'>
+        <div className='line__red' ></div>
+        <div className='line__black' >
+          <div className="circle__write">
+            <div className="circle__gray"></div>
+          </div>
+        </div>
+      </footer>
+
+    </div >
   )
 }
 
